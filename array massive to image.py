b64a30530139b859c11b57b2bdc2031d172f3f1f
MakarovDs777 <42496353+MakarovDs777@users.noreply.github.com>
2025-01-20 11:15:54 +0300

    Update array massive to image.py

--- array massive to image.py
+++ array massive to image.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import os
 
 def save_image_from_pixels(pixels):
     # Проверяем, что входные данные являются numpy-массивом
@@ -7,14 +8,15 @@ def save_image_from_pixels(pixels):
         raise ValueError("Входные данные должны быть numpy-массивом")
 
     # Проверяем, что массив имеет правильную форму
-    if len(pixels.shape)!= 3 or pixels.shape[2] not in [3, 4]:
+    if len(pixels.shape) != 3 or pixels.shape[2] not in [3, 4]:
         raise ValueError("Массив должен иметь форму (высота, ширина, количество_цветовых_каналов)")
 
     # Создаем изображение из массива пикселей
     img = Image.fromarray(pixels.astype(np.uint8))
 
-    # Сохраняем изображение на рабочий стол
-    img.save("image.png")
+    # Получаем путь к рабочему столу
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    img.save(os.path.join(desktop_path, "image.png"))
 
 # Пример использования
 pixels = np.random.randint(0, 256, size=(512, 512, 3), dtype=np.uint8)
